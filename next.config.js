f7daa281069acbee307536aba9ff4b8ff1751f05
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-12-06 23:37:23 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "starter-kit-anik-bin.vercel.app/blog", 
+      },
+      {
+        source: "/blog/:path*",
+        destination: "starter-kit-anik-bin.vercel.app/blog/:path*",
+    ];
+  },
+}
 
 module.exports = nextConfig
